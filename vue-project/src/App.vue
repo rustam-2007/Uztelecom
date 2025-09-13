248ac720e462273220b6f41de549c44c76565a00
Aziz Rozmetov <argringo97@gmail.com>
2025-09-13 15:09:35 +0500

    first comit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
-import { RouterView } from "vue-router";
-import Navbar from "./components/NavbarComponent.vue";
-import Footer from "./components/FooterComponent.vue"; // footer komponentini ham ulab oling
+import { RouterView } from 'vue-router'
+import Navbar from './components/NavbarComponent.vue'
+import Footer from './components/FooterComponent.vue' // footer komponentini ham ulab oling
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
     <!-- Navbar -->
     <Navbar />
-
     <!-- Body qismi -->
     <main class="flex-grow container mx-auto p-4">
       <RouterView />
