d6af586c56668d6c21201084404a413724c22adc
Rustam Karimjonov <rustamkarimjonov19072007@gmail.com>
2025-10-19 14:09:37 +0500

    Merge branch 'main' of https://github.com/rustam-2007/Uztelecom

---
+++ vue-project/src/components/InputComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    class="max-w-md mx-auto bg-white dark:bg-[#0f172a] p-6 rounded-2xl shadow-md space-y-5 transition-colors mb-6 duration-300"
+  >
+    <!-- Email -->
+    <div class="flex flex-col space-y-1">
+      <label for="email" class="text-sm font-medium text-gray-700 dark:text-gray-300">Email:</label>
+      <input
+        id="email"
+        type="email"
+        placeholder="example@email.com"
+        v-model="form.email"
+        class="border border-purple-400 focus:ring-2 focus:ring-purple-500 focus:outline-none rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 dark:bg-[#1e293b] dark:text-gray-100 dark:placeholder-gray-400 dark:border-gray-600"
+      />
+    </div>
+
+    <!-- Ism va familiya -->
+    <div class="flex flex-col space-y-1">
+      <label for="name" class="text-sm font-medium text-gray-700 dark:text-gray-300"
+        >Ism va familiya:</label
+      >
+      <input
+        id="name"
+        type="text"
+        placeholder="Zuxriddin Akbarov"
+        v-model="form.name"
+        class="border border-gray-300 focus:ring-2 focus:ring-purple-500 focus:outline-none rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 dark:bg-[#1e293b] dark:text-gray-100 dark:placeholder-gray-400 dark:border-gray-600"
+      />
+    </div>
+
+    <!-- Xabar -->
+    <div class="flex flex-col space-y-1">
+      <label for="message" class="text-sm font-medium text-gray-700 dark:text-gray-300"
+        >Xabar:</label
+      >
+      <textarea
+        id="message"
+        rows="3"
+        placeholder="Xabaringiz..."
+        v-model="form.message"
+        class="border border-gray-300 focus:ring-2 focus:ring-purple-500 focus:outline-none rounded-lg px-4 py-3 text-gray-700 placeholder-gray-400 resize-none dark:bg-[#1e293b] dark:text-gray-100 dark:placeholder-gray-400 dark:border-gray-600"
+      ></textarea>
+    </div>
+
+    <!-- Yuborish tugmasi -->
+    <div>
+      <button
+        @click="submitForm"
+        class="w-full h-16 rounded-2xl text-base flex flex-row justify-center items-center font-bold bg-[#174880] text-white dark:bg-blue-700 dark:hover:bg-blue-800 transition"
+      >
+        Yuborish
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+  email: '',
+  name: '',
+  message: '',
+})
+
+const submitForm = () => {
+  console.log('Yuborilgan ma’lumot:', form)
+}
+</script>
--- vue-project/src/pages/OpportunitiesView.vue
+++ vue-project/src/pages/OpportunitiesView.vue
@@ -1,75 +1,161 @@
 <template>
-  <div class="px-8">
-    <div>
-      <h1
-        class="text-xl md:text-6xl font-bold mt-20 flex justify-center items-center text-center lg:font-size-20px lg:font-size-61px"
-      >
-        1C UZTELECOM funksional <br />
-        imkoniyatlari
-      </h1>
-      <div class="flex justify-center items-center mt-10">
-        <img src="../assets/img/earth.png" alt="" />
+  <div
+    class="px-8 bg-white text-gray-800 dark:bg-[#0f172a] dark:text-gray-100 transition-colors duration-500"
+  >
+    <section>
+      <div>
+        <h1
+          class="text-xl md:text-6xl font-bold pt-12 flex justify-center items-center text-center lg:font-size-20px lg:font-size-61px dark:text-white"
+        >
+          1C UZTELECOM funksional <br />
+          imkoniyatlari
+        </h1>
+        <div class="flex justify-center items-center mt-10 w-full">
+          <img class="w-full" src="../assets/img/earth.png" alt="" />
+        </div>
       </div>
-    </div>
-    <div>
-      <p
-        class="flex justify-center items-center text-center text-base mt-20"
-        style="color: #4d5055"
-      >
-        1C: UZTELECOM — bu bulutli servis bo‘lib, “1C” dasturlarining asosiy funksiyalaridan <br />
-        foydalanish imkonini beradi — hech qanday dastur o‘rnatish yoki sozlash shart emas. <br />
-        Sizga kerak bo‘ladigani — faqat internet va brauzer xolos.
-      </p>
-      <div class="flex justify-start lg:justify-around flex-wrap gap-10 mt-20 mb-20">
-        <!-- card start  -->
-        <div class="p-6 w-full rounded-2xl md:w-[35%] lg:w-[25%]" style="background-color: #174880">
-          <div>
-            <img src="../assets/icons/card-icon.svg" alt="" />
+      <div>
+        <p
+          class="flex justify-center items-center text-center text-base mt-20 text-[#4d5055] dark:text-gray-300"
+        >
+          1C: UZTELECOM — bu bulutli servis bo‘lib, “1C” dasturlarining asosiy funksiyalaridan
+          <br />
+          foydalanish imkonini beradi — hech qanday dastur o‘rnatish yoki sozlash shart emas. <br />
+          Sizga kerak bo‘ladigani — faqat internet va brauzer xolos.
+        </p>
+        <div class="flex justify-start lg:justify-between flex-wrap gap-10 mt-20 mb-20">
+          <!-- card start  -->
+          <div
+            class="p-6 w-full rounded-2xl md:w-[35%] lg:w-[25%] bg-[#174880] dark:bg-blue-900 transition-colors duration-500"
+          >
+            <div>
+              <img src="../assets/icons/card-icon.svg" alt="" />
+            </div>
+            <br /><br /><br /><br />
+            <h1 class="text-white text-3xl font-bold">Tezkor start</h1>
+            <p class="text-gray-400 text-xl mt-4 dark:text-gray-300">
+              Bazangizni yuklab beramiz — <br />15 daqiqada ishga kirishasiz.
+            </p>
           </div>
-          <br /><br /><br /><br />
-          <h1 class="text-white text-3xl font-bold">Tezkor start</h1>
-          <p class="text-gray-400 text-xl mt-4">
-            Bazangizni yuklab beramiz — <br />15 daqiqada ishga kirishasiz.
-          </p>
-        </div>
-        <div class="p-6 w-full rounded-2xl md:w-[35%] lg:w-[25%]" style="background-color: #174880">
-          <div>
-            <img src="../assets/icons/card-icon.svg" alt="" />
+          <div
+            class="p-6 w-full rounded-2xl md:w-[35%] lg:w-[25%] bg-[#174880] dark:bg-blue-900 transition-colors duration-500"
+          >
+            <div>
+              <img src="../assets/icons/card-icon.svg" alt="" />
+            </div>
+            <br /><br /><br /><br />
+            <h1 class="text-white text-3xl font-bold">Tezkor start</h1>
+            <p class="text-gray-400 text-xl mt-4 dark:text-gray-300">
+              Bazangizni yuklab beramiz — <br />15 daqiqada ishga kirishasiz.
+            </p>
           </div>
-          <br /><br /><br /><br />
-          <h1 class="text-white text-3xl font-bold">Tezkor start</h1>
-          <p class="text-gray-400 text-xl mt-4">
-            Bazangizni yuklab beramiz — <br />15 daqiqada ishga kirishasiz.
-          </p>
-        </div>
-        <div class="p-6 w-full rounded-2xl md:w-[35%] lg:w-[25%]" style="background-color: #174880">
-          <div>
-            <img src="../assets/icons/card-icon.svg" alt="" />
+          <div
+            class="p-6 w-full rounded-2xl md:w-[35%] lg:w-[25%] bg-[#174880] dark:bg-blue-900 transition-colors duration-500"
+          >
+            <div>
+              <img src="../assets/icons/card-icon.svg" alt="" />
+            </div>
+            <br /><br /><br /><br />
+            <h1 class="text-white text-3xl font-bold">Tezkor start</h1>
+            <p class="text-gray-400 text-xl mt-4 dark:text-gray-300">
+              Bazangizni yuklab beramiz — <br />15 daqiqada ishga kirishasiz.
+            </p>
           </div>
-          <br /><br /><br /><br />
-          <h1 class="text-white text-3xl font-bold">Tezkor start</h1>
-          <p class="text-gray-400 text-xl mt-4">
-            Bazangizni yuklab beramiz — <br />15 daqiqada ishga kirishasiz.
-          </p>
+          <!-- card end  -->
         </div>
-        <!-- card end  -->
+      </div>
+    </section>
+
+    <section>
+      <div class="flex justify-center">
+        <h1 class="text-5xl font-bold text-center mt-2 dark:text-white">
+          Avtomatlashtiring, boshqaring, <br />tahlil qiling — bularning barchasi <br />
+          1C UZTELECOM yordamida!
+        </h1>
+      </div>
+    </section>
+
+    <section>
+      <div class="flex flex-col gap-10 w-full justify-start md:flex-row mt-20 mr-4">
         <div>
-          <h1 class="text-5xl font-bold text-center mt-20">
-            Avtomatlashtiring, boshqaring, <br />tahlil qiling — bularning barchasi <br />
-            1C UZTELECOM yordamida!
-          </h1>
+          <h1 class="font-bold text-3xl dark:text-white">1. Ishga tayyor — hammasi sozlangan</h1>
+          <span class="flex gap-2 mt-2">
+            <img style="color: #174880" src="../assets/icons/star-icon.svg" alt="" />
+            <p class="dark:text-gray-300">Internet va brauzer bo‘lsa kifoya.</p>
+          </span>
+          <p class="mt-2 text-[#4d5055] dark:text-gray-300">
+            Hech qanday dastur o‘rnatish shart emas — hatto elektron raqamli imzo <br />
+            kalitini ham «bulutda» saqlash mumkin, bu orqali <br />
+            siz Internet orqali hisobot topshirishingiz mumkin.
+          </p>
         </div>
         <div>
-          <h1>1. Ishga tayyor — hammasi sozlangan</h1>
-          <img style="color: #174880" src="../assets/icons/star-icon.svg" alt="" />
-          <p>Internet va brauzer bo‘lsa kifoya.</p>
-          <p>
-            Hech qanday dastur o‘rnatish shart emas — hatto elektron raqamli imzo kalitini ham
-            «bulutda» saqlash mumkin, bu orqali siz Internet orqali hisobot topshirishingiz mumkin.
-          </p>
           <img src="../assets/img/work-settings.svg" alt="" />
         </div>
       </div>
-    </div>
+    </section>
+
+    <section>
+      <div>
+        <div class="flex flex-row gap-10 w-full justify-end md:flex-row mt-20 mr-4">
+          <div>
+            <img src="../assets/img/work-settings.svg" alt="" />
+          </div>
+          <div>
+            <h1 class="font-bold text-3xl dark:text-white">
+              2. Ishni boshlash oson — 30 kun <br />
+              bepul
+            </h1>
+            <span class="flex gap-2 mt-2">
+              <p class="dark:text-gray-300">Oddiy shaklni to‘ldiring va bugunoq ishni boshlang!</p>
+            </span>
+            <p class="mt-2 text-[#4d5055] dark:text-gray-300">
+              Shaklni to‘ldirish atigi bir necha daqiqa vaqt oladi. Shundan so‘ng,<br />
+              menejerimiz siz bilan bog‘lanadi, barcha tafsilotlarni tushuntiradi va <br />tizimga
+              ulanishda yordam beradi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="flex justify-center flex-col items-center">
+        <div class="w-32">
+          <img src="../assets/icons/large-star.svg" alt="" />
+        </div>
+        <h1 class="text-4xl font-bold text-center dark:text-white">
+          O‘zingiz sinab ko‘ring — 30 kunlik bepul foydalanish imkoniyatini <br />
+          hoziroq oling!
+        </h1>
+        <p class="text-center my-4 text-[#383A3D] dark:text-gray-300">
+          Ro‘yxatdan o‘ting va 30 kun bepul foydalaning
+        </p>
+        <button
+          class="w-56 h-16 rounded-2xl text-base flex flex-row justify-center items-center font-bold bg-[#174880] text-white dark:bg-blue-700 dark:hover:bg-blue-800 transition"
+        >
+          Bepul ulanish
+          <img class="ml-3" src="../assets/icons/button-star.svg" style="width: 30px" alt="" />
+        </button>
+      </div>
+    </section>
+
+    <section>
+      <div class="mt-5">
+        <h1 class="text-4xl font-bold flex justify-center dark:text-white mb-8">
+          Bizga xabar yuboring
+        </h1>
+        <p class="text-base text-center text-[#4d5055] dark:text-gray-300">
+          Savolingiz, taklifingiz yoki 1C UZTELECOM asosidagi yechimlarimiz haqida ko‘proq
+          bilmoqchimisiz? <br />
+          Biz bilan bog‘laning — doim aloqadamiz va yordam berishga tayyormiz!
+        </p>
+        <InputComponent />
+      </div>
+    </section>
   </div>
 </template>
+
+<script setup>
+import InputComponent from '@/components/InputComponent.vue'
+</script>
